aa479ba70704bb2ae89e40015453e42be5929892
407-28-fahwiangphing <54640@yupparaj.ac.th>
2026-06-26 11:40:08 +0700

    Update digital.profile.py

--- digital.profile.py
+++ digital.profile.py
@@ -1,10 +1,10 @@
 import streamlit as st
 
 # 1. หัวข้อใหญ่: แนะนำตัวหลักและชื่อเจ้าของเว็บ
-st.title(My digital Profile)
+st.title(my digital profile)
 
 # 2. หัวข้อย่อยที่ 1: แนะนำตัว
-st.header("แนะนำตัว)
+st.header("แนะนำตัว")
 st.write("นางสาวฟ้าเวียงพิงค์ จันทร์กระจ่าง")
 st.write("ชื่อเล่น ฟ้าใส")
 st.write("เลขที่28 ม.4/7")
