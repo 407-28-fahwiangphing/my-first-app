5f10da58b3b15bf3f738f1121abc72f7a049be75
407-28-fahwiangphing <54640@yupparaj.ac.th>
2026-08-21 11:25:59 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -101,4 +101,4 @@ if st.session_state.get("is_ended", False):
     show_result_dialog(ans1, ans2)
 
 st.divider()
-st.write("นางสาวดีใจ ยิ้มแย้ม เลขที่ 5 ม.4/5")
+st.write("นางสาวฟ้าเวียงพิงค์ จันทร์กระจ่าง เลขที่ 28 ม.4/7")
