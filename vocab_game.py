b8a0847a1d6b535bbd2477394ce1d0c4287044c3
407-28-fahwiangphing <54640@yupparaj.ac.th>
2026-08-21 11:49:07 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -62,7 +62,7 @@ def show_result_dialog(ans1, ans2, ans3, ans4):
         st.error(f"❌ ข้อ 3: ยังไม่ถูกต้อง (คุณตอบ '{u_ans3}')")
 
      # ตรวจข้อ 4
-    if u_ans4 == "you":
+    if u_ans4 == "love":
         st.success("✅ ข้อ 4: ถูกต้อง")
         score += 1
     else:
