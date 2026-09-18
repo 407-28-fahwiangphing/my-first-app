3997a523962798e57e8dfcb1a9f488518ef8f1a8
407-28-fahwiangphing <54640@yupparaj.ac.th>
2026-09-18 11:36:33 +0700

    Update guessthecountry.py

--- guessthecountry.py
+++ guessthecountry.py
@@ -111,7 +111,7 @@ else:
 
     st.info(f"🏆 ได้คะแนนรวม: {score} คะแนน")
 
-    if score == 4:
+    if score == 10:
         st.success("🎉 You win!")
 else:
         st.error("💀 You lose!")
@@ -124,7 +124,7 @@ st.button("🎮 เริ่มเล่นเกม", on_click=reset_game)
 
 # 2. แถบแสดงเวลานับถอยหลัง
 if "start" in st.session_state and not st.session_state.get("is_ended", False):
-    time_left = int(30 - (time.time() - st.session_state.start))
+    time_left = int(90 - (time.time() - st.session_state.start))
 
     if time_left > 0:
         st.error(f"⏳ เหลือเวลา: {time_left} วินาที")
@@ -136,27 +136,57 @@ st.divider()
 
 # 3. ช่องรับคำตอบ (ใช้ value ผูกกับตัวแปรตรงๆ เพื่อสั่งเคลียร์ได้)
 ans1 = st.text_input(
-    "ข้อ 1: An `a _ _ l e` a day keeps the doctor away. 🍎",
+    "ข้อ 1:mexico:",
     value=st.session_state.ans1_val,
 )
 ans2 = st.text_input(
-    "ข้อ 2: Cats love to eat `f _ s h`. 🐟",
+    "ข้อ 2:egypt:",
     value=st.session_state.ans2_val,
 )
 ans3 = st.text_input(
-    "ข้อ 3: I am a `d _ g` lover. ",
+    "ข้อ 3:jp:",
     value=st.session_state.ans3_val,
 )
 ans4 = st.text_input(
-    "ข้อ 4: I `l _ _e ` you to the moon and back. ",
+    "ข้อ 4:kr:",
     value=st.session_state.ans4_val,
 )
+ans5 = st.text_input(
+    "ข้อ 5:colombia:",
+    value=st.session_state.ans5_val,
+)
+ans6 = st.text_input(
+    "ข้อ 6:portugal:",
+    value=st.session_state.ans6_val,
+)
+ans7 = st.text_input(
+    "ข้อ 7:fr:",
+    value=st.session_state.ans7_val,
+)
+ans8 = st.text_input(
+    "ข้อ 8:de:",
+    value=st.session_state.ans8_val,
+)
+ans9 = st.text_input(
+    "ข้อ 9:argentina:",
+    value=st.session_state.ans9_val,
+)
+ans10 = st.text_input(
+    "ข้อ 10:south_africa:",
+    value=st.session_state.ans10_val,
 
 # อัปเดตค่าล่าสุดเข้าตัวแปร
 st.session_state.ans1_val = ans1
 st.session_state.ans2_val = ans2
 st.session_state.ans3_val = ans3
 st.session_state.ans4_val = ans4
+st.seesion_state.ans5_val = ans5
+st.seesion_state.ans6_val = ans6
+st.seesion_state.ans7_val = ans7
+st.seesion_state.ans8_val = ans8
+st.seesion_state.ans9_val = ans9
+st.seesion_state.ans10_val = ans10
+
 
 # 4. ปุ่มส่งคำตอบ
 if "start" in st.session_state and not st.session_state.get("is_ended", False):
@@ -169,7 +199,7 @@ if "start" in st.session_state and not st.session_state.get("is_ended", False):
 
 # 5. แสดง Dialog ผลลัพธ์
 if st.session_state.get("is_ended", False):
-    show_result_dialog(ans1, ans2, ans3, ans4)
+    show_result_dialog(ans1, ans2, ans3, ans4, ans5, ans6, ans7, ans8, ans9, ans10,)
 
 st.divider()
-st.write("นางสาวฟ้าเวียงพิงค์ จันทร์กระจ่าง เลขที่ 28 ม.4/7")
+st.write("นาย ศุภกร ประสมสวย 45,น.ส.ณัฎฐวี คำลือ 14,น.ส.เกณิกา พูลทะจักร์ 21,น.ส.ฟ้าเวียงพิงค์ จันทร์กระจ่าง 28 มัธยมศึกษาปีที่ 4/7")
