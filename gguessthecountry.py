d8a6814f1296ba4936400dfcc87a3e1889facd10
407-28-fahwiangphing <54640@yupparaj.ac.th>
2026-09-24 19:47:44 +0700

    Update gguessthecountry.py

--- gguessthecountry.py
+++ gguessthecountry.py
@@ -1,7 +1,7 @@
 import time
 import streamlit as st
 
-st.title("⏱️ เกมทายประเทศ")
+st.title("⏱️ เกมทายประเทศจากอิโมจิ")
 
 # 1. กำหนดค่าเริ่มต้นใน session_state ถ้ายังไม่มี
 if "ans1_val" not in st.session_state:
